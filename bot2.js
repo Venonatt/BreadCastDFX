e17e8ffbc58c895fc02a9711e40587b3455e887c
Venonatt <42704102+Venonatt@users.noreply.github.com>
2019-03-28 14:59:11 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -88,9 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("DFX Shop
-                             
-")
+                    .setFooter("DFX Shop")
 
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
