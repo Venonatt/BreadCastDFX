d3430a761edbf723b52ce21f2eae6d61711a0653
Venonatt <42704102+Venonatt@users.noreply.github.com>
2019-03-28 15:07:14 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("Music2",{type: 'WATCHING'})
+   client.user.setActivity("BotMusic 2",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
